4da07e74f8656b6f5a77089744fe4768ba591077
massoncorlette <massoncorlette07@gmail.com>
2024-07-19 01:34:31 -0400

    setting up API url with API key given by Visual Crossing weather website so I can get data off their server

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # weatherapp
 Focusing on working with API's, asynchronous code, Promise objects
+
+https://www.visualcrossing.com/resources/documentation/weather-api/timeline-weather-api/
+
+
--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 export function test() {
   console.log('testconsole');
+
+  fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/2024-7-19?key=F9HSLGSNS2LUJYXZA84KV9H2M', {mode: 'cors'})
+  .then(function(response) {
+    console.log(response.json());
+  })
 };
